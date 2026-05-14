bda3031253d85c278bdd2a88617be5fcb9bc7ade
LL-oyiso <theo.tose.tt@gmail.com>
2026-05-14 18:53:22 +0200

    Added core models enums and EF core dbcontext

---
+++ GLMS_Monolith/Data/GlmsDbContext.cs
@@ -0,0 +1,36 @@
+﻿using GLMS_Monolith.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GLMS_Monolith.Data;
+
+public class GlmsDbContext : DbContext
+{
+    public GlmsDbContext(DbContextOptions<GlmsDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Client> Clients => Set<Client>();
+    public DbSet<Contract> Contracts => Set<Contract>();
+    public DbSet<ServiceRequest> ServiceRequests => Set<ServiceRequest>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Contract>()
+            .HasOne(c => c.Client)
+            .WithMany(c => c.Contracts)
+            .HasForeignKey(c => c.ClientId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ServiceRequest>()
+            .HasOne(sr => sr.Contract)
+            .WithMany(c => c.ServiceRequests)
+            .HasForeignKey(sr => sr.ContractId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<ServiceRequest>().Property(x => x.CostUsd).HasPrecision(18, 2);
+        modelBuilder.Entity<ServiceRequest>().Property(x => x.CostZar).HasPrecision(18, 2);
+        modelBuilder.Entity<ServiceRequest>().Property(x => x.ExchangeRateUsed).HasPrecision(18, 6);
+    }
+}
---
+++ GLMS_Monolith/Models/Enums/ContractStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace GLMS_Monolith.Models.Enums;
+
+public enum ContractStatus
+{
+    Draft = 0,
+    Active = 1,
+    Expired = 2,
+    OnHold = 3
+}
